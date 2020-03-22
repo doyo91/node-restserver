cabb2b641b111f02d7f850b5838f9e620f9fa2cb
doyo91 <doyo_91@hotmail.com>
2020-03-22 19:27:28 +0100

    Variable de entrono Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://doyo91:ZVb7WVM9s4hMv0fD@cluster0-xcfp6.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
